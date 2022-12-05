b365fa122d86e39a2d6dfa5a11c17048799db304
mrxshinji <95459307+mrxshinji@users.noreply.github.com>
2022-12-05 14:41:58 +0800

    day 5 complete

---
+++ day5/day5-part1.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const readline = require("readline");
+
+async function getHighestOfStack() {
+  const fileStream = fs.createReadStream("./day5/day5.txt");
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+  let arr = [];
+  for await (const line of rl) {
+    arr.push(line);
+  }
+
+  // determine number of stack and create a dict from 1 - N with value of []
+  const numberOfStack = (arr[0].length - 1 - 2) / 4 + 1;
+  let dict = {};
+  for (let i = 1; i <= numberOfStack; i++) {
+    dict[i] = [];
+  }
+  // return { key of 1 to n : []}
+
+  // loop through until meet digit and stop
+  // digit always start from line[1] and + 4 from there, push each digit into the array dict
+  for (line of arr) {
+    if (!/[0-9]/g.test(line)) {
+      for (let i = 1, j = 1; i < line.length; i += 4, j++) {
+        if (line[i] !== " ") dict[j].push(line[i]);
+      }
+    }
+  }
+  // return { '1': [ 'N', 'Z' ], '2': [ 'D', 'C', 'M' ], '3': [ 'P' ] }
+
+  //reverse each key to enact the scenario
+  for (const [key] of Object.entries(dict)) {
+    dict[key].reverse();
+  }
+  // return { '1': [ 'Z', 'N' ], '2': [ 'M', 'C', 'D' ], '3': [ 'P' ] } and its ready to move
+
+  // move part
+  for (line of arr) {
+    if (/move/.test(line)) {
+      // quantity , from stack , to stack = [i, j, k]
+      let extract = line.split(' ').filter((letter) => /[0-9]/g.test(letter)).map(el => parseInt(el))
+      let quantity = extract[0], from = extract[1], to = extract[2]
+      for (let i = 0; i < quantity; i++) {
+        dict[to].push(dict[from].pop())
+      }
+    }
+  }
+  console.log(dict)
+}
+
+getHighestOfStack();
+
+//     [D]
+// [N] [C]
+// [Z] [M] [P]
+//  1   2   3
